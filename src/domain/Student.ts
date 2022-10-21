4e4ab98cf34845b782b977e3af20c2fe1c6cd407
Thomas Schuster <thomasdahlind@gmail.com>
2022-10-20 23:24:08 -0300

    add courses.

--- src/domain/Student.ts
+++ src/domain/Student.ts
@@ -1,11 +1,21 @@
-import { Person } from "./Person";
 import { StudentCode } from "./StudentCode";
+import { StudentCourse } from "./StudentCourse";
+import { Course } from "./Course";
 
 export class Student {
   code: StudentCode;
+  courses: StudentCourse[];
 
-  constructor(readonly person: Person, readonly sequence?: number) {
-    this.code = new StudentCode(sequence, person.birth_date);
+  constructor(readonly id_person: number, readonly date: Date = new Date(), readonly sequence?: number) {
+    this.code = new StudentCode(sequence, date);
+    this.courses = [];
   }
   
+  addCourse(course: Course, time_studied: number )  {
+    this.courses.push(new StudentCourse(this.id_person, time_studied, course.id_course))
+  }
+
+  getCode() {
+   return this.code.getCode()
+  }
 }
